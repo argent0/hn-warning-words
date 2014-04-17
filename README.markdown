883e05dccf09b00fb132d49ee0cf7e3ca757a9fd
Aner Lucero <4rgento@gmail.com>
2014-04-17 19:57:51 -0300

    Remove word

--- README.markdown
+++ README.markdown
@@ -1,1 +1,5 @@
 This extension hihghlights a user defined list of words in hacernews comment pages
+
+## Bugs
+
+It breaks some links.
--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 /*global _:false, document:false */
 
 var user_interface = {
+    g_list_element_id_prefix: "wl-",
+    g_word_list_id: "word-list",
     g_imprecise_word_list: [
         "all",
         "always",
@@ -20,23 +22,85 @@ var user_interface = {
         "some",
         "worst"
     ],
-    list_current_words: function() {
+    remove_word_button: function(word) {
+        var button_element = document.createElement('button'),
+            t = this;
+        button_element.innerText = 'X';
+        button_element.onclick = function() {
+            var word_element = this.parentElement,
+                word_list_element = word_element.parentElement;
+            t.remove_word(word);
+            word_list_element.removeChild(word_element);
+            t.remove_word(word);
+        };
+        console.log(button_element);
+        return button_element;
+    },
+    word_element: function(word) {
+        var word_element = document.createElement('li'),
+            remove_word_button = this.remove_word_button(word);
+        word_element.innerText = word;
+        word_element.id = this.g_list_element_id_prefix + word;
+        word_element.appendChild(remove_word_button);
+        return word_element;
+    },
+    refresh_current_word_list: function() {
         var word_index,
             word_list_element = document.createElement('ul'),
-            word_element;
+            word_element,
+            word_text;
+
+        this.remove_word_list();
+
+        word_list_element.id = this.g_word_list_id;
+
         for(word_index = 0; word_index < this.g_imprecise_word_list.length; ++word_index) {
-            word_element = document.createElement('li');
-            word_element.innerText = this.g_imprecise_word_list[word_index];
+            word_text = this.g_imprecise_word_list[word_index];
+            word_element = this.word_element(word_text);
             word_list_element.appendChild(word_element);
         }
 
         document.body.appendChild(word_list_element);
+    },
+    remove_word_list: function() {
+        var word_list_element = document.getElementById(this.g_word_list_id);
+        if (word_list_element) {
+            word_list_element.parentElement.removeChild(word_list_element);
+        }
+    },
+    init: function() {
+        var add_btn = document.getElementById('btn-add');
+        var t = this;
+        add_btn.onclick = function () {
+            t.add_word("foo");
+            t.refresh_current_word_list();
+        };
+    },
+    add_word: function(word) {
+        var lower_case_word = word.toLowerCase();
+        if (this.is_word_listed(lower_case_word)) {
+            this.g_imprecise_word_list.push(lower_case_word);
+            this.g_imprecise_word_list.sort();
+        } else {
+            console.log("Word already listed!");
+        }
+    },
+    remove_word: function(word) {
+        var lower_case_word = word.toLowerCase();
+        if ( this.is_word_listed(lower_case_word) ) {
+            var word_index = this.g_imprecise_word_list.idexOf(lower_case_word);
+            this.g_imprecise_word_list.splice(word_index,1);
+        }
+    },
+    is_word_listed: function(word) {
+        return this.g_imprecise_word_list.indexOf(word.toLowerCase()) === -1;
     }
 };
 
 document.addEventListener('DOMContentLoaded', function () {
 	console.log("Loading popup");
-    user_interface.list_current_words();
+    user_interface.init();
+    user_interface.refresh_current_word_list();
 });
 
 // vim:tabstop=4 shiftwidth=4 sts=4 expandtab smartindent
