d2af95a134887e070a41fc59073d6ebf941f1830
Aner Lucero <4rgento@gmail.com>
2014-04-18 17:38:57 -0300

    Using local storage for imprecise word list

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
-/*global _:false, document:false */
+/*global _:false, document:false, chrome: false */
 
 var user_interface = {
     g_list_element_id_prefix: "wl-",
     g_word_list_id: "word-list",
     g_word_input_field_id: "word-input-text",
-    g_imprecise_word_list: [
+    g_imprecise_word_list_storage_key: "imprecise_word_list",
+    g_default_imprecise_word_list: [
         "all",
         "always",
         "bad",
@@ -23,6 +24,7 @@ var user_interface = {
         "some",
         "worst"
     ],
+    g_imprecise_word_list: [],
     remove_word_button: function(word) {
         var button_element = document.createElement('button'),
             t = this;
@@ -32,6 +34,7 @@ var user_interface = {
                 word_list_element = word_element.parentElement;
             t.remove_word(word);
             word_list_element.removeChild(word_element);
+            t.store_imprecise_word_list();
         };
         return button_element;
     },
@@ -75,7 +78,22 @@ var user_interface = {
             t.add_word(input_field_element.value);
             t.refresh_current_word_list();
             console.log(t.g_imprecise_word_list);
+            t.store_imprecise_word_list();
         };
+
+        //Initialize g_imprecise_word_list from local storage or set it to the
+        //default words
+        this.after_loading_local_storage_imprecise_word_list(
+            function(stored_imprecise_word_list) {
+            if (stored_imprecise_word_list.length > 0) {
+                console.log("Using stored imprecise word list");
+                t.g_imprecise_word_list = stored_imprecise_word_list;
+                t.refresh_current_word_list();
+            } else {
+                console.log("Using default word list");
+                this.g_imprecise_word_list = this.g_default_imprecise_word_list;
+            }
+        });
     },
     add_word: function(word) {
         var lower_case_word = word.toLowerCase();
@@ -97,6 +115,30 @@ var user_interface = {
     },
     is_word_listed: function(word) {
         return this.g_imprecise_word_list.indexOf(word.toLowerCase()) !== -1;
+    },
+    /**
+     * Retrieves the list of user-defined imprecise words from local storage
+     */
+    after_loading_local_storage_imprecise_word_list: function(onDone) {
+        var t = this;
+        chrome.storage.sync.get(this.g_imprecise_word_list_storage_key, function(items) {
+            console.log("ITEMS");
+            var stored_imprecise_word_list = items[t.g_imprecise_word_list_storage_key];
+            if (typeof stored_imprecise_word_list === 'undefined') {
+                stored_imprecise_word_list = [];
+            }
+            console.log(stored_imprecise_word_list);
+            onDone(stored_imprecise_word_list);
+        });
+    },
+    store_imprecise_word_list: function() {
+        var object = {};
+        object[this.g_imprecise_word_list_storage_key] = this.g_imprecise_word_list;
+        chrome.storage.sync.set(
+            object,
+            function() {
+                console.log("Saved imprecise word list");
+        });
     }
 };
 
